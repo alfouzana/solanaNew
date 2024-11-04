b42695ad54efe1e136fc65d03b6649f88fb9655f
Abdullah Alfouzan <alfouzana@me.com>
2024-11-04 13:02:44 +0300

    Update SolanaCreateToken.js

--- components/SolanaCreateToken.js
+++ components/SolanaCreateToken.js
@@ -99,13 +99,21 @@ const SolanaCreateToken = ({ setLoader }) => {
 
   const uploadMetadata = async (token) => {
     const data = JSON.stringify(token);
-    const response = await axios.post("https://api.pinata.cloud/pinning/pinJSONToIPFS", data, {
-      headers: {
-        pinata_api_key: process.env.PINATA_API_KEY,
-        pinata_secret_api_key: process.env.PINATA_SECRET_KEY,
-      },
-    });
-    return `https://gateway.pinata.cloud/ipfs/${response.data.IpfsHash}`;
+  
+    try {
+      const response = await axios.post("https://api.pinata.cloud/pinning/pinJSONToIPFS", data, {
+        headers: {
+          pinata_api_key: process.env.NEXT_PUBLIC_PINATA_API_KEY,
+          pinata_secret_api_key: process.env.NEXT_PUBLIC_PINATA_SECRET_KEY,
+          "Content-Type": "application/json",
+        },
+      });
+  
+      return `https://gateway.pinata.cloud/ipfs/${response.data.IpfsHash}`;
+    } catch (error) {
+      console.error("Error uploading metadata to IPFS:", error);
+      throw new Error("Failed to upload metadata to IPFS.");
+    }
   };
 
   const handleImageChange = async (event) => {
