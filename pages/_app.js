4a598dae00ca0ab100e83c69b155bc59fb4c39c2
Abdullah Alfouzan <alfouzana@me.com>
2024-11-04 12:42:33 +0300

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
 
-import React, { useMemo } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
@@ -10,9 +10,20 @@ import "@solana/wallet-adapter-react-ui/styles.css"; // Optional styles for wall
 import "../styles/styles.css";
 
 function MyApp({ Component, pageProps }) {
+  const [isClient, setIsClient] = useState(false);
   const network = WalletAdapterNetwork.Devnet;
-  
+
+  useEffect(() => {
+    // This will only run on the client side
+    setIsClient(true);
+  }, []);
+
   const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
+
+  // Render null on the server to avoid SSR issues
+  if (!isClient) {
+    return null;
+  }
 
   return (
     <ConnectionProvider endpoint={`https://api.devnet.solana.com`}>
